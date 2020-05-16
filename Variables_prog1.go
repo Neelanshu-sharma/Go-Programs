a8f13861f31bdba7f77b0b361e93b524826f75d1
“neelanshu.sharma” <neelanshu.sharma@c2fo.com>
2020-05-17 00:37:40 +0530

    add 5 files for datatypes

---
+++ Variables_prog1.go
@@ -0,0 +1,10 @@
+package main
+import "fmt"
+func main(){
+var i int;
+var f float64;
+var b bool;
+var s string;
+fmt.Printf("%T %T %T %T\n",i,f,b,s)
+    fmt.Printf("%v   %v     %v    %v",i,f,b,s)
+}
---
+++ static_var_declaration.go
@@ -0,0 +1,8 @@
+// Static Type Declaration in Go 
+package main
+import "fmt"
+func main(){
+    var x float64 = 20.0
+    fmt.Println(x)
+    fmt.Printf("Type of variable x is: %T\n", x)    
+}
